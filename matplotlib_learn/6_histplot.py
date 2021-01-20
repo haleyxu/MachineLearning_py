6abcd2e460a0f39d627d58d6874888039739bd94
Haleyxu <xuyifeiemail@163.com>
2021-01-20 16:31:25 +0800

    添加文档

--- matplotlib_learn/6_histplot.py
+++ matplotlib_learn/6_histplot.py
@@ -1,5 +1,7 @@
-from matplotlib import pyplot as plt
 import random
+
+from matplotlib import pyplot as plt
+
 #设置字体
 plt.rcParams['font.sans-serif']=['SimSun'] #用来正常显示中文标签
 plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
